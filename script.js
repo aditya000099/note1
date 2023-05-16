ba185d46811a0491a41ae4d1d144ffe46842fd75
aditya000099 <adi.agg099@gmail.com>
2023-05-16 16:07:28 +0530

    second

--- script.js
+++ script.js
@@ -85,7 +85,8 @@ function reminder(noteId, title) {
   }, fminutes);
 }
 function alertrrem(noteId, title) {
-  alert("Reminder for " + title);
+  // alert("Reminder for " + title);
+  const notification = new Notification("Reminder for " + title);
 }
 
 function showMenu(elem) {
@@ -145,3 +146,31 @@ addBtn.addEventListener("click", (e) => {
     closeIcon.click();
   }
 });
+
+
+//net
+function notifyMe() {
+  if (!("Notification" in window)) {
+    // Check if the browser supports notifications
+    alert("This browser does not support desktop notification");
+  } else if (Notification.permission === "granted") {
+    // Check whether notification permissions have already been granted;
+    // if so, create a notification
+    // const notification = new Notification("Hi there!");
+    // …
+  } else if (Notification.permission !== "denied") {
+    // We need to ask the user for permission
+    Notification.requestPermission().then((permission) => {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        const notification = new Notification("Notifications Enabled Successfully");
+        // …
+      }
+    });
+  }
+
+  // At last, if the user has denied notifications, and you
+  // want to be respectful there is no need to bother them anymore.
+}
+
+notifyMe();
